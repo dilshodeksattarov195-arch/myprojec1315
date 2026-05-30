dec3dde443ce35ffc5bbed9aba3035819a9ae24b
DILSHODBEK <dilshodsattar26@gmail.com>
2026-05-30 21:55:42 +0500

    Initial commit: setup main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+import sys
+import json
+
+class configFalculateWorker:
+    def __init__(self):
+        self.buffer = []
+    def append_metric(self, val):
+        self.buffer.append(val)
+        return len(self.buffer)
+
+if __name__ == '__main__':
+    obj = configFalculateWorker()
+    print("Worker engine initialized.")
